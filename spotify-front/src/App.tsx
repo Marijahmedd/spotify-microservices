7af69e189ff5e3efae16543031fe861f15c17aeb
Marij <muhammadmarijahmed@gmail.com>
2025-07-03 02:13:37 +0500

    initialized frontend with shadcn and tailwind

---
+++ spotify-front/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Button>Click here</Button>
+    </>
+  );
+}
+
+export default App;
