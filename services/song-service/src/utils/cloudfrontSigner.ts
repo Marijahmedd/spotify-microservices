6d249c4717132b50c2a824661ef63c3269789817
Marij <muhammadmarijahmed@gmail.com>
2025-07-18 20:27:21 +0500

    modified .env to include Cloudfront_private_key

--- services/song-service/src/utils/cloudfrontSigner.ts
+++ services/song-service/src/utils/cloudfrontSigner.ts
@@ -1,13 +1,15 @@
 import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
 import path from "path";
 import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+// const privateKeyPath = path.join(
+//   __dirname,
+//   "../keys/cloudfront-private-key.pem"
+// );
+// const privateKey = fs.readFileSync(privateKeyPath, "utf8");
 
-const privateKeyPath = path.join(
-  __dirname,
-  "../keys/cloudfront-private-key.pem"
-);
-const privateKey = fs.readFileSync(privateKeyPath, "utf8");
-
+const privateKey = process.env.CLOUDFRONT_PRIVATE_KEY!
 export function generateSignedCloudFrontUrl(key: string) {
   const fullUrl = `${process.env.CLOUDFRONT_DOMAIN}/${key}`;
   console.log(fullUrl);
