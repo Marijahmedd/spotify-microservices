d618514a7e3e891746e570faa1aba77becc6fd15
Marij <muhammadmarijahmed@gmail.com>
2025-07-22 07:01:18 +0500

    testing trivy

--- k8s/stress-test.js
+++ k8s/stress-test.js
@@ -2,15 +2,15 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export let options = {
-  vus: 1000,              // Virtual users
-  duration: '60s',      // Test duration
+  vus: 1000,              
+  duration: '60s',     
 };
 
 export default function () {
-  let res = http.get('https://api.spotify.marij.me/api/song/stream/d9cb767a-346d-4682-8a88-3e9afd7c8748'); // <-- Replace this
+  let res = http.get('https://api.spotify.marij.me/api/song/stream/d9cb767a-346d-4682-8a88-3e9afd7c8748'); 
   check(res, {
     'status was 200': (r) => r.status === 200,
   });
-  sleep(1);  // Think time
+  sleep(1);
 }
 
